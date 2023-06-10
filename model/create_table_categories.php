baf616d28977295a842b527eb3349f8186c85ad1
noura mamdouh <nouramamdouh25@gmail.com>
2023-06-10 14:10:26 -0700

    create table categories with data in it

--- model/create_table_categories.php
+++ model/create_table_categories.php
@@ -6,9 +6,14 @@ include "dbconnection.php";
 
 $db= connect_to_database($dbuser,$dbpassword,$dbhost,$dbname);
 
+try{
+$create_category_query="create table `cafe`.`categories`(`id` int auto_increment primary key , `name` varchar(255))";
 
+$db->query($create_category_query);
 
-
+}catch(Exception $e){
+echo  $e->getmessage();
+}
 
 
 
